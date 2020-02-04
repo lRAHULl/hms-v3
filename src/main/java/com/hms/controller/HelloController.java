b68de13c4e52cbd79ff82fde862bbd778d7ec833
lRAHULl <rahulpadmaraju16@gmail.com>
2020-02-04 14:14:15 +0530

    sample end point for pipeline testing

--- src/main/java/com/hms/controller/HelloController.java
+++ src/main/java/com/hms/controller/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@GetMapping("/hi")
+	@GetMapping("/hello")
 	public String hello() {
+		return "hello World";
+	}
+
+	@GetMapping("/hi")
+	public String hi() {
 		return "hello World";
 	}
 }
